c511923febe2268349eda29d83fd748115654e2f
ETauzi <eliott.tauzi@etu.univ-orleans.fr>
2026-04-01 12:23:56 +0200

    bibli list

--- src/Livre.java
+++ src/Livre.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Livre{
     private int nbpage;
     private Debut pDeb;
     private Fin pFin;
     private List<Page> pages;
-
     
     
 }
