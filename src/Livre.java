50bdd88c319c470a0a82d7b07732a48605576a9e
ETauzi <eliott.tauzi@etu.univ-orleans.fr>
2026-05-06 12:23:44 +0200

    debut d'idée pour les enigmes

--- src/Livre.java
+++ src/Livre.java
@@ -1,11 +1,35 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+
 
 public class Livre{
     private int nbpage;
+    private static Map<Integer,List<Map<String,String>>> enigmes = new HashMap<>();
     private Debut pDeb;
     private Fin pFin;
     private List<Page> pages;
+
+
+    public Livre(int nbpage, Debut pDeb, Fin pFin) {
+        this.nbpage = nbpage;
+        this.pDeb = pDeb;
+        this.pFin = pFin;
+        this.pages = new ArrayList<>();
+    }
+
+    private void fill(){
+        List<Map<String,String>> facile = new ArrayList<>();
+        List<Map<String,String>> moyen = new ArrayList<>();
+        List<Map<String,String>> dif = new ArrayList<>();
+        enigmes.put(3, facile);
+        enigmes.put(6, moyen);
+        enigmes.put(9, dif);
+    }
+    
+    
     
     
 }
